15571eeb7aba16d5c29c6605d9baa5db7c2679c4
Jamozed <jake@omkov.net>
2020-09-17 15:38:45 +1200

    Add zoom and potion inventory offset remover

---
+++ src/main/java/net/omkov/lepton/Lepton.java
@@ -0,0 +1,20 @@
+package net.omkov.lepton;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class Lepton implements ClientModInitializer {
+	public static KeyBinding bindingZoom;
+	
+	public static final double zoomFactor = 4.0;
+	public static boolean zoom = false;
+	public static boolean zoomSmoothCamera = false;
+	
+	@Override
+	public void onInitializeClient() {
+		bindingZoom = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.lepton.zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "key.categories.misc"));
+	}
+}
---
+++ src/main/java/net/omkov/lepton/mixin/GameRendererMixin.java
@@ -0,0 +1,28 @@
+package net.omkov.lepton.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.render.GameRenderer;
+import net.omkov.lepton.Lepton;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+	private MinecraftClient mc = MinecraftClient.getInstance();
+	
+	@Redirect(method = "getFov", at = @At(value = "FIELD", target = "Lnet/minecraft/client/options/GameOptions;fov:D", ordinal = 0))
+	private double getFov(GameOptions options) {
+		if (Lepton.bindingZoom.isPressed()) {
+			if (!Lepton.zoom) {
+				Lepton.zoom = true; Lepton.zoomSmoothCamera = options.smoothCameraEnabled;
+				options.smoothCameraEnabled = true; mc.worldRenderer.scheduleTerrainUpdate();
+			} return options.fov / Lepton.zoomFactor;
+		}
+		else if (Lepton.zoom) {
+			Lepton.zoom = false; options.smoothCameraEnabled = Lepton.zoomSmoothCamera;
+			mc.worldRenderer.scheduleTerrainUpdate();
+		} return options.fov;
+	}
+}
