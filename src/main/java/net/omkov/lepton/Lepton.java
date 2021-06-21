aae9231e8f7dc09017751849ef143f38d54fec5f
Jamozed <jake@omkov.net>
2021-06-21 14:19:20 +1200

    Remove zoom function

--- src/main/java/net/omkov/lepton/Lepton.java
+++ src/main/java/net/omkov/lepton/Lepton.java
@@ -24,22 +24,16 @@ public class Lepton implements ClientModInitializer {
 	public static KeyBinding bindElytra = null;
 	public static KeyBinding bindFly = null;
 	public static KeyBinding bindNoFall = null;
-	public static KeyBinding bindZoom = null;
 	
 	private static ModuleElytra mElytra = new ModuleElytra();
 	private static ModuleFlight mFlight = new ModuleFlight();
 	private static ModuleNoFall mNoFall = new ModuleNoFall();
-	
-	public static final double zoomFactor = 4.0;
-	
-	public static boolean zoomEnabled = false;
 	
 	@Override
 	public void onInitializeClient() {
 		bindElytra = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.lepton.elytra", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.cheats"));
 		bindFly    = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.lepton.fly",    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.cheats"));
 		bindNoFall = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.lepton.nofall", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.categories.cheats"));
-		bindZoom   = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.lepton.zoom",   InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C,       "key.categories.misc"));
 		
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (bindElytra.wasPressed()) { mElytra.toggle(); }
